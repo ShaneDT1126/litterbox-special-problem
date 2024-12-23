3391014698c2a89b6af29a54965942206cc2d765
Shane <shanedelatorre2412@gmail.com>
2024-12-23 22:14:41 +0800

    Add new file for token counter

---
+++ src/utils/tokenCounter.js
@@ -0,0 +1,28 @@
+const { encodingForModel } = require("js-tiktoken");
+
+class TokenCounter {
+    constructor(model = "gpt-3.5-turbo") {
+        this.encoding = encodingForModel(model);
+    }
+
+    countTokens(text) {
+        return this.encoding.encode(text).length;
+    }
+
+    countMessageTokens(messages) {
+        let totalTokens = 0;
+        for (const message of messages) {
+            totalTokens += 4; // Every message follows <im_start>{role/name}\n{content}<im_end>\n
+            for (const [key, value] of Object.entries(message)) {
+                totalTokens += this.countTokens(value);
+                if (key === "name") {
+                    totalTokens -= 1; // Role is always required and always 1 token
+                }
+            }
+        }
+        totalTokens += 2; // Every reply is primed with <im_start>assistant
+        return totalTokens;
+    }
+}
+
+module.exports = TokenCounter;
